095b8f4b9fc6245da003e4e08e199b056597ae32
lakshay <lakshay22csu100@ncuindia.edu>
2025-07-23 17:45:55 +0530

    Initial PHP

---
+++ BeginPHP/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP Website</h1>
+    <?php
+
+    echo "This is done using php";
+
+    // This is a comment in PHP and will not be executed 
+
+    echo "<br>"; // Line break in HTML
+
+
+    // PHP is a dynamic scripting language i.e. we don't need to declare the type of variable
+    $name = "Lakshay Goel"; // Variable declaration
+    $age = 20; // Another variable declaration
+    echo $name; // Output the value of $name
+
+    
+    $a = 10; // Assigning value to variable $a
+
+    ?>
+</body>
+</html>
